60173943f30f7d37db2eb14243f641ea74f0fb44
J.Kettula <jkettula@ycp.edu>
2022-12-09 17:32:46 -0500

    Final Commit

--- src/spell_checker/core.clj
+++ src/spell_checker/core.clj
@@ -1,79 +1,39 @@
 (ns spell-checker.core
-  (:require  [clojure.string  :as str])
-  (:use clojure.core.matrix))
-  
+  (:require  [clojure.string  :as str]))
+
+;;Opens a file to be read
 (defn read-file [path]
-  "This function reads and returns the content of a file with a given path"
   (slurp path))
 
+;;Converts the lines of the dictionary to a set
+(defn to-dict [wordstring]
+  (set (clojure.string/split-lines wordstring)))
 
-(defn to-words [wordstring]
-  "Convert a given sequence of newline delimetered string into a set of words"
-  (set  (map  #(str/trim %)   (str/split-lines  wordstring))))
+(def dict
+  (to-dict (read-file "src/spell_checker/dictionary.txt")))
 
-;; Our dictionary
-(def words  (-> "/usr/share/dict/words"
-                (read-file)
-                (to-words)))
-
-
-
+;;Checks if the word already exists in the file
 (defn correct-word? [word]
-  "Check if a word is in our dictionary"
-  (contains? words word))
+  (contains? dict word))
 
+;;TODO
+(defn damerau–levenshtein [word dictionary]
+  (+ 0 (rand 2)))
 
+;; (defn damerau–levenshtein [word dictionary]
+;;   (let [s1 word]))
 
-;; An Implementation of the damerau–levenshtein inspired by
-;;https://stackoverflow.com/users/3687048/mark
+;;Uses min-key to find the closest match in the dictionary
+(defn min-distance [word]
+  (apply min-key (partial damerau–levenshtein word) dict))
 
-(defn damerau–levenshtein [fstr sstr]
-  (let [fstr-len (count fstr)
-        sstr-len (count sstr)
-        mtx (new-matrix :ndarray (inc fstr-len) (inc sstr-len))]
-    (mset! mtx 0 0 0)
-    (dotimes [i fstr-len]
-      (mset! mtx (inc i) 0 (inc i)))
-    (dotimes [j sstr-len]
-      (mset! mtx 0 (inc j) (inc j)))
-    (dotimes [i fstr-len]
-      (dotimes [j sstr-len]
-        (let [i+ (inc i) j+ (inc j)
-              i- (dec i) j- (dec j)
-              cost (if (= (.charAt fstr i)
-                          (.charAt sstr j))
-                     0 1)]
-          (mset! mtx i+ j+
-                 (min (inc (mget mtx i j+))
-                      (inc (mget mtx i+ j))
-                      (+ (mget mtx i j) cost)))
-          (if (and (pos? i) (pos? j)
-                   (= (.charAt fstr i)
-                      (.charAt sstr j-))
-                   (= (.charAt fstr i-)
-                      (.charAt sstr j)))
-            (mset! mtx i+ j+
-                   (min (mget mtx i+ j+)
-                        (+ (mget mtx i- j-) cost)))))))
-    (mget mtx fstr-len sstr-len)))
+(defn main [& args]
+  (println "Enter a word to spell check :")
+  (let [word  (read-line)]
+    (if (correct-word? word)
+      (println word "Is spelled correctly. ")
+      (do
+        (println word "Is misspelled. ")
+        (println "Did you mean -> " (min-distance word))))))
 
-
-
-
-(defn minimun-distance [word]
-  (apply min-key (partial damerau–levenshtein word) words))
-
-
-(defn -main
-  
-  [& args]
-  (time
-   (let [_ (println "Enter word possibly misspelled :")
-         word  (read-line)]
-     (if (correct-word? word)
-       (println word  "Is a correct word")
-
-       (do
-         (println word "Is wrong word ")
-         (println "Did you mean -> " (minimun-distance word))))))
-)
+(main)
